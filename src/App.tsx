6df8de7f128ebf950fc6d4501ee67e4ebd5dc5af
shcodenick <wojtek.kloc@gmail.com>
2023-07-19 15:40:03 +0200

    materialui, used it for i18n buttons, changes css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useTransition } from 'react';
 import './App.css';
 import { useTranslation } from 'react-i18next';
+import Button from '@mui/material/Button';
  
 
 const lngs = {
@@ -17,7 +18,7 @@ function App() {
       <header className="App-header">
         <div>
           {Object.keys(lngs).map((lng) => (
-            <button key={lng} onClick={() => i18n.changeLanguage(lng)} disabled={i18n.resolvedLanguage === lng}>{ lngs[lng as LangKey].nativeName }</button>
+            <Button variant="contained" key={lng} onClick={() => i18n.changeLanguage(lng)} disabled={i18n.resolvedLanguage === lng}>{ lngs[lng as LangKey].nativeName }</Button>
           ))}
         </div>
         <a
