60a30231564bcb5596e68216facf3803302887c1
shcodenick <wojtek.kloc@gmail.com>
2023-08-04 15:16:47 +0200

    component mapper and dynamic routing

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,21 +5,39 @@ import {
   } from "react-router-dom";
 import InvoicesList from './InvoicesList';
 import InvoiceForm from './InvoiceForm';
-import PageNotFound
- from './PageNotFound';
+import PageNotFound from './PageNotFound';
   
 const Content = () => {
 
+  interface ComponentMap {
+    [key: string]: React.ComponentType<any>;
+  }
+
+  const componentMap: ComponentMap = {
+      'InvoicesList': InvoicesList,
+      'InvoiceForm': InvoiceForm,
+      'PageNotFound': PageNotFound,
+  }
+
+  function getComponentByName(name:string) {
+    const Component = componentMap[name];
+    if (Component) {
+      return <Component />;
+    } else {
+      return null;
+    }
+  }
+
   const routing_map = {
-    '/': InvoicesList,
-    '/invoice': InvoiceForm,
-    '*': PageNotFound,
+    '/': 'InvoicesList',
+    '/invoice': 'InvoiceForm',
+    '*': 'PageNotFound',
   };
 
   return (
       <Routes>
-        {Object.entries(routing_map).map(([path, Component]) => (
-          <Route key={path} path={path} element={<Component/>} />
+        {Object.entries(routing_map).map(([path, componentName]) => (
+          <Route key={path} path={path} element={getComponentByName(componentName)} />
         ))}
       </Routes>
   )
